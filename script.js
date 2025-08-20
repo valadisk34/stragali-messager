125937e22cce6c9d66082a9020b9d02476a38974
valadis.k <140048577+valadisk34@users.noreply.github.com>
2025-08-20 17:59:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const serverURL = "https://script.google.com/macros/s/AKfycbzT2aO645aQvUjEyPTTXWzRahPaUM-7vhsJOTyaZ1iCsjf5uhPICSwvw-KVs9ugnYXYWQ/exec";
+const serverURL = "https://script.google.com/macros/s/AKfycbzCVkjsXI8tXAMxDfk3GMRsVRHO2Q_Ep0zERjZt2TwIu_43aiEUNitHb4bYU65WZKqByw/exec";
 let currentUser = null;
 let balanceInterval = null;
 
@@ -170,6 +170,7 @@ function confirmDelete() {
 
 // --- Auto-refresh μηνυμάτων
 setInterval(loadMessages, 3000);
+
 
 
 
